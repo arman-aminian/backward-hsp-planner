10675e1ca41698f1d6346500a7414fee28f2422b
Arman Aminian <arman.aminian78@gmail.com>
2023-02-09 15:02:45 +0330

    action class added.

--- classes/predicate.py
+++ classes/predicate.py
@@ -2,6 +2,7 @@ class Predicate(object):
     def __init__(self, name, n_args):
         self.name = name
         self.n_args = n_args
+        self.args = [''] * n_args
 
     def __str__(self):
-        return self.name + ":" + self.n_args
+        return self.name + ":" + self.n_args + ' : ' + [arg for arg in self.args]
