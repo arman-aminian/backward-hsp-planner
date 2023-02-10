992765162b086579e913eb9a3ae084d4a7bea444
Arman Aminian <arman.aminian78@gmail.com>
2023-02-10 18:44:25 +0330

    feat: replace with objects function added.

--- classes/predicate.py
+++ classes/predicate.py
@@ -7,5 +7,11 @@ class Predicate(object):
         else:
             self.args = args
 
+    def replace_with_objects(self, objects_mapping):
+        args = [objects_mapping[arg] for arg in self.args]
+        return Predicate(name=self.name,
+                         n_args=self.n_args,
+                         args=args)
+
     def __str__(self):
         return self.name + ":" + str(self.n_args) + ' : ' + ', '.join([arg for arg in self.args])
